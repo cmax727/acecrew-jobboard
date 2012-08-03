2a0667dd677088a65a6c90d78ef7040e9bc315f1
sasya8080 <sasya8080@gmail.com>
2012-08-03 16:03:53 +0800

    sasya-created

---
+++ site/modules/acecrew/theme/acecrew-diary-events.js
@@ -0,0 +1,11 @@
+calendar_popup_sms_click = function(baseurl,call_nid){
+    $url = baseurl + "/acecrew/calendar-popup/sms/" + call_nid;
+    options = {
+        url : $url,
+        width: 300,
+        height: 400,
+        autoFit: true
+    }
+    Drupal.modalFrame.open(options);
+    return false;
+}
