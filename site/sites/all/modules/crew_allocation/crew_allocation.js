49fd569afb386f3a48adbc7eb800fae5b41dd182
antonk831 <antonk831@gmail.com>
2012-08-08 10:13:00 +0300

    small change from Anton
    Signed-off-by: antonk831 <antonk831@gmail.com>

--- site/sites/all/modules/crew_allocation/crew_allocation.js
+++ site/sites/all/modules/crew_allocation/crew_allocation.js
@@ -1,46 +1,12 @@
+$(document).ready(function() {
+    $('#edit-update').click(function() {
+        $("#acecrew_calendar_output", window.parent.document).html('<img class="loading-gif" src="/modules/acecrew/theme/ajax-loader.gif" />');
+        forward = $('#acecrew_calendar_forward_days', window.parent.document).val();
+        $.get(Drupal.settings.basePath + "acecrew/calendar/" + $('#edit-date-timer-datepicker-popup-0', window.parent.document).val() + "/" + forward, function(data){
+            var result = Drupal.parseJson(data);
+            $("#acecrew_calendar_output", window.parent.document).html(result.html);
+            window.close();
+        });
+    })
+});
 
-
-(function ($) {
-
-Drupal.behaviors.crewAllocation = function() {
-  $('.crew-allocation-child:not(.crew-allocation-processed)').addClass('crew-allocation-processed').click(function() {
-    var element = this;
-
-    // This is our onSubmit callback that will be called from the child window
-    // when it is requested by a call to modalframe_close_dialog() performed
-    // from server-side submit handlers.
-    function onSubmitCallbackExample(args, statusMessages) {
-      // Display status messages generated during submit processing.
-      if (statusMessages) {
-        // $('.crew-allocation-messages').hide().html(statusMessages).show('slow');
-        window.location.reload();
-      }
-    }
-
-    // Hide the messages are before opening a new dialog.
-    $('.crew-allocation-messages').hide('fast');
-
-    // Build modal frame options.
-    var modalOptions = {
-      url: $(element).attr('href'),
-      autoFit: true,
-      onSubmit: onSubmitCallbackExample
-    };
-
-    // Try to obtain the dialog size from the className of the element.
-    var regExp = /^.*crew-allocation-size\[\s*([0-9]*\s*,\s*[0-9]*)\s*\].*$/;
-    if (typeof element.className == 'string' && regExp.test(element.className)) {
-      var size = element.className.replace(regExp, '$1').split(',');
-      modalOptions.width = parseInt(size[0].replace(/ /g, ''));
-      modalOptions.height = parseInt(size[1].replace(/ /g, ''));
-    }
-
-    // Open the modal frame dialog.
-    Drupal.modalFrame.open(modalOptions);
-
-    // Prevent default action of the link click event.
-    return false;
-  });
-};
-
-})(jQuery);
