b779193a4f9acad4d6298ed2a96b3d40f3e2d2d0
Anton <antonk831@gmail.com>
2012-08-09 18:17:25 +0300

    right side bar from Anton
    Signed-off-by: Anton <antonk831@gmail.com>

--- site/modules/acecrew/theme/acecrew-diary-events.js
+++ site/modules/acecrew/theme/acecrew-diary-events.js
@@ -12,13 +12,21 @@ var calendar_popup_sms_click = function(baseurl,call_nid){
 
 var udpate_status_onclick = function(){
     var types = $(".supplements-type");
-    var args = '';
+    var args = $('#job-session-id').val();
     
     types.each(function() {
-        args += $(this).val() + ":" + $(this).attr("id") + ":";    
+        args +=  ":" + $(this).attr("id")+ ":" + $(this).val();    
     });
     
     $.get(Drupal.settings.basePath + "acecrew/calendar/set-status/" + args, function(data){
-        alert("congratulation! your work is successed.");
+        $("#acecrew_calendar_output").html('<img class="loading-gif" src="' + Drupal.settings.basePath + Drupal.settings.acecrew.basepath + '/theme/ajax-loader.gif" />');
+        forward = $('#acecrew_calendar_forward_days').val();
+        $("#block-block-2").hide();
+        $("#block-block-2").html("<div class=\"block-content clear-block prose\"><span class=\"icon-info\"></span><p>Click on the load call button at the bottom of each call, to load more information</p></div>");
+        $("#block-block-2").fadeIn(1000);
+        $.get(Drupal.settings.basePath + "acecrew/calendar/" + $('#edit-date-timer-datepicker-popup-0').val() + "/" + forward, function(data){
+            var result = Drupal.parseJson(data);
+            $("#acecrew_calendar_output").html(result.html);
+        });
     });
 }
