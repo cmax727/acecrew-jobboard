18bec6265af91f58413a53b98ca2ad9dcb88c61b
Anton <antonk831@gmail.com>
2012-08-09 13:23:23 +0300

    no change
    Signed-off-by: Anton <antonk831@gmail.com>

--- site/modules/acecrew/theme/acecrew-calendar.js
+++ site/modules/acecrew/theme/acecrew-calendar.js
@@ -22,6 +22,9 @@ $(document).ready(function() {
     $('#edit-date-submit').click(function() {
         $("#acecrew_calendar_output").html('<img class="loading-gif" src="' + Drupal.settings.basePath + Drupal.settings.acecrew.basepath + '/theme/ajax-loader.gif" />');
         forward = $('#acecrew_calendar_forward_days').val();
+        $("#block-block-2").hide();
+        $("#block-block-2").html("<div class=\"block-content clear-block prose\"><span class=\"icon-info\"></span><p>Click on the load call button at the bottom of each call, to load more information</p></div>");
+        $("#block-block-2").fadeIn(1000);
         $.get(Drupal.settings.basePath + "acecrew/calendar/" + $('#edit-date-timer-datepicker-popup-0').val() + "/" + forward, function(data){
             var result = Drupal.parseJson(data);
             $("#acecrew_calendar_output").html(result.html);
